3656876f80735c8e2ff1e35f2c97e8a036d6253d
Lucas <lukitis2@gmail.com>
2021-08-21 02:35:36 -0300

    Configuracion inicial

---
+++ update_all.sh
@@ -0,0 +1,28 @@
+#update xsetroot function
+function run_update {
+    date_string="$(cat /home/alialucas7/.scripts/dwm/savedata/date_string.txt)"
+#	volume_string="$(cat /home/alialucas7/.scripts/dwm/savedata/volume_string.txt)"
+	battery_string="$(cat /home/alialucas7/.scripts/dwm/savedata/battery_string.txt)"
+	#internet_status_string="$(cat /home/alialucas7/.scripts/dwm/savedata/internet_status_string.txt)"
+#	bright_string="$(cat /home/alialucas7/.scripts/dwm/savedata/brightness_string.txt)"
+	#weather_string="$(cat /home/alialucas7/.scripts/dwm/savedata/weather_string.txt)"
+	#disk_use_string="$(cat /home/alialucas7/.scripts/dwm/savedata/disk_use_string.txt)"
+	#cpu_string="$(cat /home/alialucas7/.scripts/dwm/savedata/cpu_string.txt)"
+	final_string="$battery_string|$date_string"
+	#$weather_string|$disk_use_string|$internet_status_string||$date_string"
+	compare_string="$(cat /home/alialucas7/.scripts/dwm/savedata/final_string.txt)"
+	#only update the gui when a new string is made
+	if [ "$final_string" = "$compare_string" ]; then
+		#debug
+		#echo "no up" | tee ~/.scripts/dwm/savedata/up_no.txt
+		exit 0
+	else
+	
+                             xsetroot -name  "$final_string" 
+     	echo $final_string | tee ~/.scripts/dwm/savedata/final_string.txt
+		#echo "up" | tee ~/.scripts/dwm/savedata/up_no.txt
+		
+	fi
+}
+
+run_update
